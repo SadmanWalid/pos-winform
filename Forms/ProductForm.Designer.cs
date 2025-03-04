5ecac36ad64a070022988c61b74d2e21a7d68201
Md. Sadman Walid <sadman.walid@egeneration.co>
2025-03-04 15:34:28 +0600

    Refactor project structure and add category checkboxes to ProductForm.

--- Forms/ProductForm.Designer.cs
+++ Forms/ProductForm.Designer.cs
@@ -29,100 +29,41 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBoxProductDescription = new System.Windows.Forms.RichTextBox();
             this.textBoxProductName = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.labelImage = new System.Windows.Forms.Label();
+            this.labelStock = new System.Windows.Forms.Label();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.labelProductDescription = new System.Windows.Forms.Label();
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.labelAddProduct = new System.Windows.Forms.Label();
+            this.panelCatagory = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.checkBox2);
-            this.panel1.Controls.Add(this.checkBox1);
-            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.panelCatagory);
+            this.panel1.Controls.Add(this.richTextBoxProductDescription);
             this.panel1.Controls.Add(this.textBoxProductName);
-            this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.labelImage);
+            this.panel1.Controls.Add(this.labelStock);
+            this.panel1.Controls.Add(this.labelPrice);
+            this.panel1.Controls.Add(this.labelProductDescription);
+            this.panel1.Controls.Add(this.labelProductName);
+            this.panel1.Controls.Add(this.labelAddProduct);
             this.panel1.Location = new System.Drawing.Point(14, 15);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(284, 395);
+            this.panel1.Size = new System.Drawing.Size(284, 449);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // richTextBoxProductDescription
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(66, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Add Product";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 49);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Product Name";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 100);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Product Description";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 187);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Catagory";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(23, 236);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(31, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Price";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(23, 288);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Stock";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(23, 330);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(84, 13);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "Image (Optional)";
+            this.richTextBoxProductDescription.Location = new System.Drawing.Point(26, 116);
+            this.richTextBoxProductDescription.Name = "richTextBoxProductDescription";
+            this.richTextBoxProductDescription.Size = new System.Drawing.Size(208, 48);
+            this.richTextBoxProductDescription.TabIndex = 8;
+            this.richTextBoxProductDescription.Text = "";
             // 
             // textBoxProductName
             // 
@@ -131,41 +72,75 @@
             this.textBoxProductName.Size = new System.Drawing.Size(208, 20);
             this.textBoxProductName.TabIndex = 7;
             // 
-            // richTextBox1
+            // labelImage
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(26, 116);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(208, 48);
-            this.richTextBox1.TabIndex = 8;
-            this.richTextBox1.Text = "";
+            this.labelImage.AutoSize = true;
+            this.labelImage.Location = new System.Drawing.Point(23, 419);
+            this.labelImage.Name = "labelImage";
+            this.labelImage.Size = new System.Drawing.Size(84, 13);
+            this.labelImage.TabIndex = 6;
+            this.labelImage.Text = "Image (Optional)";
             // 
-            // checkBox1
+            // labelStock
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(26, 203);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(85, 17);
-            this.checkBox1.TabIndex = 9;
-            this.checkBox1.Text = "Main Course";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.labelStock.AutoSize = true;
+            this.labelStock.Location = new System.Drawing.Point(23, 377);
+            this.labelStock.Name = "labelStock";
+            this.labelStock.Size = new System.Drawing.Size(35, 13);
+            this.labelStock.TabIndex = 5;
+            this.labelStock.Text = "Stock";
             // 
-            // checkBox2
+            // labelPrice
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(117, 203);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(85, 17);
-            this.checkBox2.TabIndex = 10;
-            this.checkBox2.Text = "Main Course";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Location = new System.Drawing.Point(23, 325);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(31, 13);
+            this.labelPrice.TabIndex = 4;
+            this.labelPrice.Text = "Price";
             // 
-            // Form1
+            // labelProductDescription
+            // 
+            this.labelProductDescription.AutoSize = true;
+            this.labelProductDescription.Location = new System.Drawing.Point(23, 100);
+            this.labelProductDescription.Name = "labelProductDescription";
+            this.labelProductDescription.Size = new System.Drawing.Size(100, 13);
+            this.labelProductDescription.TabIndex = 2;
+            this.labelProductDescription.Text = "Product Description";
+            // 
+            // labelProductName
+            // 
+            this.labelProductName.AutoSize = true;
+            this.labelProductName.Location = new System.Drawing.Point(23, 49);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(75, 13);
+            this.labelProductName.TabIndex = 1;
+            this.labelProductName.Text = "Product Name";
+            // 
+            // labelAddProduct
+            // 
+            this.labelAddProduct.AutoSize = true;
+            this.labelAddProduct.Location = new System.Drawing.Point(23, 17);
+            this.labelAddProduct.Name = "labelAddProduct";
+            this.labelAddProduct.Size = new System.Drawing.Size(66, 13);
+            this.labelAddProduct.TabIndex = 0;
+            this.labelAddProduct.Text = "Add Product";
+            // 
+            // panelCatagory
+            // 
+            this.panelCatagory.AutoScroll = true;
+            this.panelCatagory.Location = new System.Drawing.Point(21, 186);
+            this.panelCatagory.Name = "panelCatagory";
+            this.panelCatagory.Size = new System.Drawing.Size(221, 127);
+            this.panelCatagory.TabIndex = 9;
+            // 
+            // ProductForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(992, 418);
+            this.ClientSize = new System.Drawing.Size(992, 476);
             this.Controls.Add(this.panel1);
-            this.Name = "Form1";
+            this.Name = "ProductForm";
             this.Text = "Products";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -176,17 +151,15 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label labelAddProduct;
+        private System.Windows.Forms.Label labelProductDescription;
+        private System.Windows.Forms.Label labelProductName;
+        private System.Windows.Forms.Label labelImage;
+        private System.Windows.Forms.Label labelStock;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.RichTextBox richTextBoxProductDescription;
         private System.Windows.Forms.TextBox textBoxProductName;
-        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Panel panelCatagory;
     }
 }
 
--- Forms/ProductForm.cs
+++ Forms/ProductForm.cs
@@ -15,8 +15,48 @@ namespace POS
         public ProductForm()
         {
             InitializeComponent();
+            LoadCatagoryCheckBoxes();
         }
 
-       
+        private void LoadCatagoryCheckBoxes()
+        {
+            // later fetch the catagories from the database
+            List<string> items = new List<string>
+            {
+                "Main Course",
+                "Salad",
+                "Soups",
+                "Combo Meals",
+                "Sides",
+                "Pasta"
+            };
+
+            GroupBox groupBox = new GroupBox
+            {
+                Text = "Catagory",
+                Location = new System.Drawing.Point(0, 0),
+                AutoSize = true
+            };
+
+            int yPosition = groupBox.Location.Y + this.labelPrice.Height + 10;
+
+            foreach (string item in items)
+            {
+                CheckBox checkBox = new CheckBox
+                {
+                    Text = item,
+                    Location = new System.Drawing.Point(10, yPosition),
+                    AutoSize = true
+                };
+
+                groupBox.Controls.Add(checkBox);
+                yPosition += 20;    
+
+            }
+
+           
+            // Add the groupBox to the panel
+            this.panelCatagory.Controls.Add(groupBox);
+        }
     }
 }
